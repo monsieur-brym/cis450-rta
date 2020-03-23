cdc338b8c23b24448ed8a60f9575531ef7691989
atrevin8 <60982142+atrevin8@users.noreply.github.com>
2020-03-22 17:16:42 -0700

    Update USAA.py
    added 3 tweets for every topic based on ranking system of most similar tweets

--- USAA.py
+++ USAA.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import operator
 pd.set_option("display.max_colwidth", 200)
 
 #from sklearn.datasets import fetch_20newsgroups
@@ -15,7 +16,7 @@ pd.set_option("display.max_colwidth", 200)
 #dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
 #documents = dataset.data
 #len(documents)
-dataset = pd.read_csv(r"C:\Users\allyt\Documents\Spring2020\CIS450\cis450-rta-master\cis450-rta-master\tweetset.csv", sep =',')
+dataset = pd.read_csv(r'C:\Users\allyt\Documents\Spring2020\CIS450\cis450-rta-master\cis450-rta-master\tweetset.csv', encoding='latin1',sep= ",")
 
 data = dataset.Tweets
 twitter_df = pd.DataFrame({'document':data})
@@ -36,7 +37,7 @@ tokenized_doc = twitter_df['clean_doc'].apply(lambda x: x.split())
 
 # remove stop-words
 tokenized_doc = tokenized_doc.apply(lambda x: [item for item in x if item not in stop_words])
-
+numtweet = len(tokenized_doc)
 # de-tokenization
 detokenized_doc = []
 for i in range(len(twitter_df)):
@@ -47,7 +48,7 @@ twitter_df['clean_doc'] = detokenized_doc
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-vectorizer = TfidfVectorizer(stop_words='english', 
+vectorizer = TfidfVectorizer(stop_words=['english'], 
 max_features= 1000, # keep top 1000 terms 
 max_df = 0.5, 
 smooth_idf=True)
@@ -59,7 +60,7 @@ X.shape # check shape of the document-term matrix
 from sklearn.decomposition import TruncatedSVD
 
 # SVD represent documents and terms in vectors 
-svd_model = TruncatedSVD(n_components=20, algorithm='randomized', n_iter=100, random_state=122)
+svd_model = TruncatedSVD(n_components=5, algorithm='randomized', n_iter=100, random_state=122)
 
 svd_model.fit(X)
 
@@ -69,8 +70,41 @@ terms = vectorizer.get_feature_names()
 
 for i, comp in enumerate(svd_model.components_):
     terms_comp = zip(terms, comp)
-    sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=True)[:7]
+    sorted_terms = sorted(terms_comp, key= lambda x:x[1], reverse=True)[:5]
     print("Topic "+str(i)+": ")
     for t in sorted_terms:
         print(t[0])
         print(" ")
+        
+    topic=[]
+    for i in sorted_terms:
+        topic.append(i[0])
+        
+    simlist = []
+    
+    for j in range(numtweet):
+        Tweetvec = tokenized_doc[j]
+        sim = 0
+        if topic[0] in Tweetvec:
+                sim+=1    
+        if topic[1] in Tweetvec:
+                sim+=.75
+        if topic[2] in Tweetvec:
+                sim+=.50
+        if topic[3] in Tweetvec:
+                sim+=.25
+        if topic[4] in Tweetvec:
+                sim+=.1  
+        vecsim=(j,sim)
+        simlist.append(vecsim)
+        
+    simlist.sort(key=operator.itemgetter(1), reverse =1)
+    
+    print('Top 3 tweets')
+    for t in range(3):     
+        tweet=simlist[t][0]
+        print()
+        print(data[tweet])
+            
+            
+        
